03ff24c7eab1248216d16bf4c3e002518db62dd6
giostam <gstamateris@gmail.com>
2016-06-17 16:51:53 +0300

    Added map values and two way transliteration

--- src/main/java/com/stam/excellatin/ExcelLatin.java
+++ src/main/java/com/stam/excellatin/ExcelLatin.java
@@ -8,16 +8,21 @@ package com.stam.excellatin;
 import com.ibm.icu.text.Transliterator;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.text.Normalizer;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -30,34 +35,88 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  */
 public class ExcelLatin {
 
+    private static final List<String> validOptions = new ArrayList<>();
+
+    static {
+        validOptions.add("-L");
+        validOptions.add("-G");
+        validOptions.add("-d");
+    }
+
     public static void main(String[] args) {
-        int optionNumber = 0;
-        String option = null;
+        List<String> options = new ArrayList<>();
+        int startIndex = 0;
         for (String arg : args) {
-            if (arg.startsWith("-")) {
-                optionNumber++;
-                option = arg;
+            if (validOptions.contains(arg)) {
+                options.add(arg);
+                startIndex++;
             }
         }
-        
-        if (args[0].equals("-h") || option > 1 || args.length == 0) {
-            System.out.println("usage: ExcelLatin [option] filenameIn filenameOut columnNames...");
-            System.out.println("option:");
-            System.out.println("\t-L\tTo Latin");
-            System.out.println("\t-G\tTo Greek");
+
+        if (args[0].equals("-h") || args.length < 3) {
+            System.out.println("usage: ExcelLatin [options] filenameIn filenameOut columnNames...");
+            System.out.println("options:");
+            System.out.println("\t-L\tto Latin (default)");
+            System.out.println("\t-G\tto Greek");
+            System.out.println("\t-d\tdon't deaccent");
+            System.out.println("\t-h\thelp");
         } else {
-            String fileNameIn = args[0];
-            String fileNameOut = args[1];
+            boolean greekToLatin = false;
+            boolean latinToGreek = false;
+            Transliterator transliterator = null;
+            if ((!options.contains("-L") && !options.contains("-G")) || options.contains("-L")) {
+                transliterator = Transliterator.getInstance("Greek-Latin/UNGEGN");
+                System.out.println("\nTransliterating Greek to Latin");
+                greekToLatin = true;
+            } else if (options.contains("-G")) {
+                transliterator = Transliterator.getInstance("Latin-Greek/UNGEGN");
+                System.out.println("\nTransliterating Latin to Greek");
+                latinToGreek = true;
+            }
+
+            if (transliterator == null) {
+                System.out.println("Not a valid option for the transliteration language");
+                return;
+            }
+
+            boolean deAccent = true;
+            if (options.contains("-d")) {
+                deAccent = false;
+                System.out.println("Will not deaccent");
+            }
+
+            String fileNameIn = args[startIndex];
+            String fileNameOut = args[startIndex + 1];
             List<String> columnNames = new ArrayList<>();
-            System.out.println("Columns to transliterate\n---------------");
-            for (int i = 2; i < args.length; i++) {
+            System.out.println("\nColumns to transliterate\n---------------------------");
+            for (int i = startIndex + 2; i < args.length; i++) {
                 columnNames.add(args[i]);
                 System.out.println(args[i]);
             }
             System.out.println("\n");
 
             try {
-                Workbook wb = WorkbookFactory.create(new File(fileNameIn));
+                File file = new File(fileNameIn);
+                if (!file.exists()) {
+                    System.out.println("The file " + fileNameIn + " was not found");
+                    return;
+                }
+
+                Map<String, String> mapTransformations = new HashMap<>();
+                Scanner sc = new Scanner(new FileReader("map.txt"));
+                while (sc.hasNextLine()) {
+                    String greekEntry = sc.next();
+                    String latinEntry = sc.next();
+
+                    if (greekToLatin) {
+                        mapTransformations.put(greekEntry, latinEntry);
+                    } else if (latinToGreek) {
+                        mapTransformations.put(latinEntry, greekEntry);
+                    }
+                }
+
+                DataFormatter formatter = new DataFormatter();
+                Workbook wb = WorkbookFactory.create(file);
 
                 Workbook newWb = null;
                 if (wb instanceof HSSFWorkbook) {
@@ -75,8 +134,8 @@ public class ExcelLatin {
 
                     Row row = sheet.getRow(0);
                     for (Cell cell : row) {
-                        cell.setCellType(Cell.CELL_TYPE_STRING);
-                        if (cell.getStringCellValue() == null || cell.getStringCellValue().trim().equals("")) {
+                        String cellVal = formatter.formatCellValue(cell);
+                        if (cellVal == null || cellVal.trim().equals("")) {
                             break;
                         }
 
@@ -96,10 +155,18 @@ public class ExcelLatin {
                         } else {
                             for (Cell cell : rowIn) {
                                 cell.setCellType(Cell.CELL_TYPE_STRING);
-                                String cellVal = cell.getStringCellValue();
+                                String cellVal = formatter.formatCellValue(cell);
                                 String cellNewVal = cellVal;
                                 if (idxs.contains(cell.getColumnIndex()) && cellVal != null) {
-                                    cellNewVal = toAscii(cellVal);
+                                    if (mapTransformations.containsKey(cellVal)) {
+                                        cellNewVal = mapTransformations.get(cellVal);
+                                    } else {
+                                        if (deAccent) {
+                                            cellNewVal = deAccent(transliterator.transform(cellVal));
+                                        } else {
+                                            cellNewVal = transliterator.transform(cellVal);
+                                        }
+                                    }
                                 }
                                 Cell cellOut = rowOut.createCell(cell.getColumnIndex());
                                 cellOut.setCellValue(cellNewVal);
@@ -124,11 +191,5 @@ public class ExcelLatin {
     private static String deAccent(String str) {
         String nfdNormalizedString = Normalizer.normalize(str, Normalizer.Form.NFD);
         return diactiticalMarksPattern.matcher(nfdNormalizedString).replaceAll("");
-    }
-
-    private static final Transliterator toLatin = Transliterator.getInstance("Any-Latin");
-
-    public static String toAscii(String str) {
-        return deAccent(toLatin.transform(str));
     }
 }
